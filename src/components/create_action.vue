b8cfd75bd1bb4558054ed6d8b9e6300e3ffda0d1
femi <joshuaobello95@gmail.com>
2022-06-19 17:56:58 -0500

    Got rid of default vue stuff, created task creator, component for task

---
+++ src/components/create_action.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import {ref} from 'vue'
+    const emit = defineEmits(["creation"])
+
+
+    const task_name_input = ref(null)
+    function creation() {
+        if(task_name_input.value != null)
+        {
+            emit("creation",task_name_input.value.value)
+            task_name_input.value.value=""
+        }
+    }
+</script>
+
+<template>
+    <label for="task_name">Task Name </label>
+    <input type="text" id="task_name" placeholder="type task name here" ref="task_name_input">
+    <br />
+    <button @click="creation">create task</button>
+</template>
---
+++ src/components/task.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import { ref } from 'vue'
+    const props = defineProps(['name'])
+</script>
+
+<template>
+    <p>{{ name }}</p>
+</template>
