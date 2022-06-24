a7e219602130adbee4878af222fa923742304ae8
femi <joshuaobello95@gmail.com>
2022-06-24 09:31:48 -0500

    Working on Task managment

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,17 @@
   import create_action from "./components/create_action.vue"
   import task from "./components/task.vue"
 
-  const tasks = ref([])
+  const todo = ref([])
 </script>
 
 <template>
 <h1>nlogn</h1>
-<task v-for="t in tasks" :name="t.title" />
-<create_action @creation="(task_name)=>{tasks.push({title: task_name})}"/>
+<h2>todo</h2>
+<template v-for="t in todo">
+	<task :name="t.title" />
 </template>
+<h2>done</h2>
+<create_action @creation="(task_name)=>{todo.push({title: task_name})}"/>
+</template>
+
+<!-- TODO: create switching from todo task done -->
--- src/components/task.vue
+++ src/components/task.vue
@@ -1,8 +1,11 @@
 <script setup>
-    import { ref } from 'vue'
-    const props = defineProps(['name'])
+    import { ref, watch } from 'vue'
+    const props = defineProps(['name','checked'])
+	const emit = defineEmits([])
 </script>
 
 <template>
-    <p>{{ name }}</p>
+    <span>{{ name }}</span>
+	<input type="checkbox" v-model="props.checked">
+	<br>
 </template>
